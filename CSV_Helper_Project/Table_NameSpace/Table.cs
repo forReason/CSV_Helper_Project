cc5a25de1b473c342332b898a51f9d1d66513a80
julian b <julian.bechtold@outlook.com>
2022-05-16 11:27:56 +0200

    - unified format. - can now convert gate io files

---
+++ CSV_Helper_Project/Table_NameSpace/Table.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_Helper_Project
+{
+    public partial class Table
+    {
+        public Table(bool hasHeaders = false)
+        {
+            this.HasHeaders = hasHeaders;
+        }
+        
+        List<string> Headers = new List<string>();
+        List<List<string>> CSV_Table = new List<List<string>>();
+        /// <summary>
+        /// the number of records in the table
+        /// </summary>
+        public int Length { get { return CSV_Table.Count; } }
+        /// <summary>
+        /// the number of columns in the table
+        /// </summary>
+        public int ColumnLength
+        {
+            get
+            {
+                if (this.HasHeaders) return Headers.Count;
+                else if (CSV_Table.Count == 0) return 0;
+                else return CSV_Table[0].Count;
+            }
+        }
+        private bool _HasHeaders;
+        /// <summary>
+        /// returns if the table has headers. When setting, Header row will be automatically removed from Data and moved to header variable or vice versa.
+        /// </summary>
+        public bool HasHeaders
+        {
+            get
+            {
+                return _HasHeaders;
+            }
+            set
+            {
+                _HasHeaders = value;
+                if (value == true && _HasHeaders == false)
+                {
+                    if (CSV_Table.Count > 0)
+                    {
+                        Headers = CSV_Table[0];
+                        CSV_Table.RemoveAt(0);
+                    }
+                }
+                else if (value == false && _HasHeaders == true)
+                {
+                    if (Headers.Count > 0)
+                    {
+                        CSV_Table.Insert(0, Headers);
+                    }
+                    Headers = new List<string>();
+
+                }
+            }
+        }
+        
+        
+        
+        
+    }
+}
---
+++ CSV_Helper_Project/Table_NameSpace/TableSearch.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_Helper_Project
+{
+    public partial class Table
+    {
+        /// <summary>
+        /// Finds the first record based on a search string and the column to search after
+        /// </summary>
+        /// <param name="lookupString"></param>
+        /// <param name="columnName"></param>
+        /// <returns></returns>
+        public string[] SearchRecord(LookupValue lookup)
+        {
+            if (!HasHeaders) throw new FieldAccessException("Table has no Headers!");
+            int columnIndex = GetColumnIndex(lookup.ColumnName);
+            return SearchRecord(GetLookupIndexValue(lookup));
+        }
+        /// <summary>
+        /// Finds the first record based on a search string and the column index which should be matched
+        /// </summary>
+        /// <param name="lookupString"></param>
+        /// <param name="columnIndex"></param>
+        /// <returns></returns>
+        public string[] SearchRecord(LookupIndexValue lookup)
+        {
+            if (lookup.ColumnIndex < 0) return null;
+            foreach (List<string> record in CSV_Table)
+            {
+                if (lookup.SearchString == record[lookup.ColumnIndex])
+                {
+                    return record.ToArray();
+                }
+            }
+            return null;
+        }
+        public string[] SearchRecord(LookupIndexValue[] lookupValues)
+        {
+            foreach (LookupIndexValue lookup in lookupValues)
+            {
+                if (lookup.ColumnIndex < 0)
+                {
+                    return null;
+                }
+            }
+
+            foreach (List<string> record in CSV_Table)
+            {
+                bool correct = true;
+                foreach (LookupIndexValue lookup in lookupValues)
+                {
+                    if (lookup.SearchString != record[lookup.ColumnIndex])
+                    {
+                        correct = false;
+                        break;
+                    }
+                }
+                if (correct) return record.ToArray();
+            }
+            return null;
+        }
+        public string[] SearchRecord(LookupValue[] lookupValues)
+        {
+            List<LookupIndexValue> indexLookups = new List<LookupIndexValue>();
+            foreach (LookupValue lookupVal in lookupValues)
+            {
+                int columnIndex = GetColumnIndex(lookupVal.ColumnName);
+                if (columnIndex < 0) return null;
+                indexLookups.Add(new LookupIndexValue(lookupVal.SearchString, columnIndex));
+            }
+            return SearchRecord(indexLookups.ToArray());
+        }
+    }
+}
