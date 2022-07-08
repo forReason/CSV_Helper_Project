f17c37d1c5e277495635f61c9248d6426b3f65e9
Julian B <bechti44@googlemail.com>
2022-07-08 17:38:06 +0200

    added unit tests

---
+++ CSV_Helper_UnitTests/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_Helper_UnitTests
+{
+    internal static class Constants
+    {
+        internal static readonly Encoding[] Encodings = { Encoding.UTF8, Encoding.Unicode, Encoding.UTF32, Encoding.ASCII, Encoding.Default, Encoding.BigEndianUnicode, Encoding.UTF7 };
+    }
+}
---
+++ CSV_Helper_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSV_Helper_UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSV_Helper_UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a6130da-1b75-4ef5-94bd-5d7f2a06a374")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
